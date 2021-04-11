049f9600455684af7084296cb9b7e20674a01d31
Ather <atharmuhammad6@gmail.com>
2021-04-12 01:40:52 +0530

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+    NewListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
